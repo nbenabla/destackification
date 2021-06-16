1ad8bf4c554c244c99c8cdb58e8874de3cb1ee90
HTalbott <51212230+HTalbott@users.noreply.github.com>
2021-06-16 14:32:38 -0700

    Added testing file

---
+++ BerghAlgorithms/src/.ipynb_checkpoints/BerghAlgorithms-checkpoint.jl
@@ -0,0 +1,9 @@
+module BerghAlgorithms
+include("StackyFan.jl")
+#Right now, StackyFan.jl contains the module's code -- should bring StackyFan here
+export BerghA
+
+# Write your package code here.
+
+
+end
